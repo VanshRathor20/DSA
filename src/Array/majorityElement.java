c6d847338eb203386b19ff634e9b65101f978866
VanshRathor20 <vanshrathor5887@gmail.com>
2025-11-13 17:48:01 +0530

    better solution of majority element in an array

--- src/Array/majorityElement.java
+++ src/Array/majorityElement.java
@@ -1,6 +1,7 @@
 package Array;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class majorityElement {
     //brute
@@ -20,10 +21,23 @@ public class majorityElement {
         return -1;
     }
 
-
+    //better
+    public static int better(int[] arr){
+        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+            if(map.get(i)>(n/2)){
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         int[] arr={2,2,3,3,2,2};
         System.out.println("brute :"+(brute(arr)));
+        System.out.println("brute :"+(better(arr)));
     }
+
 }
