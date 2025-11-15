d8882c4c9606f42f280a6bf52d4e84672896685b
VanshRathor20 <vanshrathor5887@gmail.com>
2025-11-15 12:01:11 +0530

    optimal solution of maximum sub array

--- src/Array/maximumSubArraySum.java
+++ src/Array/maximumSubArraySum.java
@@ -15,8 +15,25 @@ public class maximumSubArraySum {
         return maxi;
     }
 
+    //optimal   kanade algorithm
+    public static int optimal(int[] arr){
+            int n=arr.length;
+            int sum=0;
+            int maxi=Integer.MIN_VALUE;
+            for(int i=0;i<n;i++){
+                sum+=arr[i];
+
+                maxi = Math.max(maxi, sum);
+                if(sum<0){
+                    sum=0;
+                }
+            }
+            return maxi;
+    }
+
     public static void main(String[] args) {
         int[] arr={-2,1,-3,4,-1,2,1,-5,4};
         System.out.println("brute :"+ brute(arr));
+        System.out.println("optimal :"+ optimal(arr));
     }
 }
