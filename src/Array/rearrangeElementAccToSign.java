ea73f68715acec21e7423819c4d4a378ee711423
VanshRathor20 <vanshrathor5887@gmail.com>
2025-11-18 21:30:49 +0530

    brute advanced numbers are not equal arrange element alternate according to their sign.

--- src/Array/rearrangeElementAccToSign.java
+++ src/Array/rearrangeElementAccToSign.java
@@ -44,6 +44,41 @@ public class rearrangeElementAccToSign {
         return anss;
     }
 
+
+    // IF THE POSITIVE & NEGATIVE ARE NOT EQUAL
+        static void rearrange(ArrayList<Integer> arr) {
+            ArrayList<Integer> pos = new ArrayList<>();
+            ArrayList<Integer> neg = new ArrayList<>();
+
+            // Separate positive and negative numbers
+            for (int i = 0; i < arr.size(); i++) {
+                if (arr.get(i) >= 0)
+                    pos.add(arr.get(i));
+                else
+                    neg.add(arr.get(i));
+            }
+
+            int posIdx = 0, negIdx = 0;
+            int i = 0;
+
+            // Place positive and negative numbers alternately
+            // in the original array
+            while (posIdx < pos.size() && negIdx < neg.size()) {
+                if (i % 2 == 0)
+                    arr.set(i++, pos.get(posIdx++));
+                else
+                    arr.set(i++, neg.get(negIdx++));
+            }
+
+            // Append remaining positive numbers (if any)
+            while (posIdx < pos.size())
+                arr.set(i++, pos.get(posIdx++));
+
+            // Append remaining negative numbers (if any)
+            while (negIdx < neg.size())
+                arr.set(i++, neg.get(negIdx++));
+        }
+
     public static void main(String[] args) {
         int[] arr = {3, 1, -2, -5, 2, -4};
 
@@ -59,6 +94,11 @@ public class rearrangeElementAccToSign {
 
         int[] result2=optimal(arr);
         System.out.println("optimal :"+Arrays.toString(result2));
+
+        ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1, 2, 3, -4, -1, 4));
+        rearrange(arr1);
+        for (int i = 0; i < arr1.size(); i++)
+            System.out.print(arr1.get(i) + " ");
 
     }
 }
