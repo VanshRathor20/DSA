a10d4cb4fd7780714effa826c8704be882f7e7fb
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-09 23:23:01 +0530

    search element in rotated duplicates array

--- src/BinarySearch/searchElemInRotatedArray.java
+++ src/BinarySearch/searchElemInRotatedArray.java
@@ -31,8 +31,45 @@ public class searchElemInRotatedArray {
         return -1;
     }
 
+    // for duplicates
+    public static boolean searchDuplicates(int[] nums, int target) {
+        int low=0, high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low=low-1;
+                high=high+1;
+                continue;
+            }
+
+            if(nums[low]<=nums[mid]){
+                if(nums[mid]>target && nums[low]<=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+
+            else{
+                if(nums[mid]<target && nums[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        int[] arr={7,8,9,1,2,3,4,5,6};
+        int[] arr={7,8,9,1,2,3,4,5,6,8,8};
         System.out.println(search(arr,8));
+        System.out.println(searchDuplicates(arr,8));
     }
 }
