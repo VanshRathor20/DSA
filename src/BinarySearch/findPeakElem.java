28a851a38038952fc5eaa735b7794e388bb4407b
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-11 23:26:49 +0530

    find peak element using BS(optimal)

--- src/BinarySearch/findPeakElem.java
+++ src/BinarySearch/findPeakElem.java
@@ -10,9 +10,32 @@ public class findPeakElem {
         }
         return -1;
     }
+    public static int findPeakElement(int[] nums) {
+        int n=nums.length;
+        if (n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+
+        int low=1, high=n-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid]>nums[mid-1]){
+                low=mid+1;
+            }
+            else if(nums[mid]>nums[mid+1]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
        int[] arr={1,2,3,4,5,6,7,8,5,1};
         System.out.println( peakElement(arr));
+        System.out.println(findPeakElement(arr));
     }
 }
