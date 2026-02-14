5cb7dc74d56456034f9907450267444dabe0dab7
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-15 00:43:42 +0530

    find smallest divisor optimal

--- src/BinarySearch/smallDivisor.java
+++ src/BinarySearch/smallDivisor.java
@@ -30,10 +30,37 @@ public class smallDivisor {
         }
         return max;
     }
+        public static int smallestDivisorOptimal(int[] nums, int threshold) {
 
+            int low = 1;
+            int high = findMax(nums);
+            int ans = high;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                int sum = calculateSum(nums, mid);
+
+                if (sum <= threshold) {
+                    ans = mid;       // possible answer
+                    high = mid - 1;  // try smaller divisor
+                } else {
+                    low = mid + 1;   // need bigger divisor
+                }
+            }
+
+            return ans;
+        }
+    private static int calculateSum(int[] nums, int divisor) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += (num + divisor - 1) / divisor;  // ceil division
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         System.out.println(smallestDivisor(arr, 4));
-//        System.out.println(smallestDivisorOptimal(arr, 4));
+        System.out.println(smallestDivisorOptimal(arr, 4));
     }
 }
