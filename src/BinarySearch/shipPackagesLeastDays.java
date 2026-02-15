46b97ed995b1820cc8428d7da283877dcb0a9d1d
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-15 18:02:26 +0530

    least capacity to ship packages with D days optimal solution

--- src/BinarySearch/shipPackagesLeastDays.java
+++ src/BinarySearch/shipPackagesLeastDays.java
@@ -42,8 +42,25 @@ public class shipPackagesLeastDays {
         return -1;
     }
 
+    //optimal
+    public static int shipWithinDaysOptimal(int[] weights, int days) {
+        int low=findMax(weights);
+        int high=findSum(weights);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int daysRequired=findDaysReguired(weights,mid);
+            if(daysRequired<=days){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6,7,8,9};
         System.out.println(shipWithinDays(arr,5));
+        System.out.println(shipWithinDaysOptimal(arr,5));
     }
 }
