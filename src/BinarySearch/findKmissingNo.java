267e1da8dfb18075e75d40c8e5cfc9aba6016d4a
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-17 22:49:08 +0530

    find kth missing number optimal

--- src/BinarySearch/findKmissingNo.java
+++ src/BinarySearch/findKmissingNo.java
@@ -9,8 +9,26 @@ public class findKmissingNo {
         return k;
     }
 
+
+    //optimal
+    public static int findKthPositive(int[] arr, int k) {
+        int low=0, high=arr.length-1;
+        while(low<=high){
+            int  mid=low+(high-low)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+
+            }else{
+                high=mid-1;
+            }
+        }
+        return high+1+k;
+    }
     public static void main(String[] args) {
         int[] arr={2,3,4,7,11};
         System.out.println(brute(arr,5));
+        System.out.println(findKthPositive(arr,5));
+
     }
 }
