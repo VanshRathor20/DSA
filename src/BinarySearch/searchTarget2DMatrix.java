60db3e887a22c487da04bc038aebcd80f87505af
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-25 23:45:16 +0530

    search element in 2D matrix better solution

--- src/BinarySearch/searchTarget2DMatrix.java
+++ src/BinarySearch/searchTarget2DMatrix.java
@@ -13,6 +13,57 @@ public class searchTarget2DMatrix {
         }
         return false;
     }
+
+    //better
+    public boolean better(int[] nums, int target) {
+        // Get the length of the array
+        int n = nums.length;
+
+        // Initialize low and high pointers
+        int low = 0, high = n - 1;
+
+        // Perform binary search
+        while (low <= high) {
+            // Calculate the middle index
+            int mid = (low + high) / 2;
+
+            // If the middle element is the target, return true
+            if (nums[mid] == target)
+                return true;
+
+                // If target is greater, search in the right half
+            else if (target > nums[mid])
+                low = mid + 1;
+
+                // Otherwise, search in the left half
+            else
+                high = mid - 1;
+        }
+
+        // Return false if target is not found
+        return false;
+    }
+
+    // Function to search for target in a 2D matrix
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // Get the number of rows
+        int n = matrix.length;
+
+        // Get the number of columns
+        int m = matrix[0].length;
+
+        // Traverse each row
+        for (int i = 0; i < n; i++) {
+            // Check if target could be in this row
+            if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
+                // Perform binary search on this row
+                return better(matrix[i], target);
+            }
+        }
+
+        // Return false if target is not found
+        return false;
+    }
     public static void main(String[] args) {
         // Define a 2D matrix
         int[][] matrix = {
