c8772b61787fd72657fde1d9513a89cf5011da79
VanshRathor20 <vanshrathor5887@gmail.com>
2026-02-25 23:46:40 +0530

    search element in 2D matrix optimal solution

--- src/BinarySearch/searchTarget2DMatrix.java
+++ src/BinarySearch/searchTarget2DMatrix.java
@@ -64,6 +64,43 @@ public class searchTarget2DMatrix {
         // Return false if target is not found
         return false;
     }
+
+    //optimal
+    public boolean optimal(int[][] matrix, int target) {
+        // Get the number of rows
+        int n = matrix.length;
+
+        // Get the number of columns
+        int m = matrix[0].length;
+
+        // Set initial binary search range
+        int low = 0, high = n * m - 1;
+
+        // Perform binary search
+        while (low <= high) {
+            // Calculate middle index
+            int mid = (low + high) / 2;
+
+            // Map 1D index to 2D coordinates
+            int row = mid / m;
+            int col = mid % m;
+
+            // Check if target is found
+            if (matrix[row][col] == target)
+                return true;
+
+                // Discard left half
+            else if (matrix[row][col] < target)
+                low = mid + 1;
+
+                // Discard right half
+            else
+                high = mid - 1;
+        }
+
+        // Target not found
+        return false;
+    }
     public static void main(String[] args) {
         // Define a 2D matrix
         int[][] matrix = {
