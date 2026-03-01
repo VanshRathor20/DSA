3b94dc5e440cfedfc20f6be2546f5e04a22ca9df
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-01 15:53:47 +0530

    sum of N natural number by using formula

--- src/Recursion/Basic_recursion/SumOfNno.java
+++ src/Recursion/Basic_recursion/SumOfNno.java
@@ -9,11 +9,20 @@ public class SumOfNno {
         }
         return sum;
     }
-
+    // by using formula
+    public static int formula(int[] arr){
+        int n=arr.length;
+//        int sum=0;
+//        for(int i=0;i<=n;i++){
+//            sum=n*(n+1)/2;
+//        }
+        return (n*(n+1)/2);
+    }
     public static void main(String[] args) {
         SumOfNno sol=new SumOfNno();
         int[] arr={1,2,3,4,5};
         System.out.println(sol.brute(arr));
+        System.out.println(sol.formula(arr));
     }
 }
 
