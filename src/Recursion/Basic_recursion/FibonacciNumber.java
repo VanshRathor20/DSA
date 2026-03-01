e1730a6895dec61ce57febd4801086b7c424453a
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-01 22:11:39 +0530

    optimal recursion of fibonacci series

--- src/Recursion/Basic_recursion/FibonacciNumber.java
+++ src/Recursion/Basic_recursion/FibonacciNumber.java
@@ -8,9 +8,21 @@ public class FibonacciNumber {
         int slast=fib(n-2);
         return last+slast;
     }
-
+    // Most Optimal (Space Optimized) 🚀
+    public static int optimal(int n){
+        if(n<=1) return n;
+        int last=1;
+        int slast=0;
+        for(int i=2;i<=n;i++){
+            int curr=last+slast;
+            slast=last;
+            last=curr;
+        }
+        return slast;
+    }
     public static void main(String[] args) {
         FibonacciNumber sol=new FibonacciNumber();
         System.out.println(sol.fib(5));
+        System.out.println(sol.optimal(5));
     }
 }
