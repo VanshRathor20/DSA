f150e9881e6498c836cb1fc8194eb2b81de6958d
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-01 16:09:26 +0530

    sum of N natural number by using recursive

--- src/Recursion/Basic_recursion/SumOfNno.java
+++ src/Recursion/Basic_recursion/SumOfNno.java
@@ -4,8 +4,8 @@ public class SumOfNno {
     //brute
     public static int brute(int[] arr){
         int sum=0,n=arr.length;
-        for(int i=0;i<=n;i++){
-            sum+=i;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
         }
         return sum;
     }
@@ -18,11 +18,18 @@ public class SumOfNno {
 //        }
         return (n*(n+1)/2);
     }
+
+    // by using recursive function
+    public static int recursive(int n){
+        if(n==0) return 0;
+        return n+recursive(n-1);
+    }
     public static void main(String[] args) {
         SumOfNno sol=new SumOfNno();
         int[] arr={1,2,3,4,5};
         System.out.println(sol.brute(arr));
         System.out.println(sol.formula(arr));
+        System.out.println(sol.recursive(5));
     }
 }
 
