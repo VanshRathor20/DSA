3b1595d64a40cb9beeba120f8125c9d442c022db
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-05 23:44:47 +0530

    max consecutive ones better solution

--- src/SlidingWindow/MaxConsecutiveOnesIII.java
+++ src/SlidingWindow/MaxConsecutiveOnesIII.java
@@ -20,9 +20,30 @@ public class MaxConsecutiveOnesIII {
         return maxLen;
     }
 
+    //better
+    public static int better(int[] arr,int k){
+        int maxLen=0, n=arr.length,l=0,r=0, zero=0;
+        while(r<n){
+            if(arr[r]==0) zero++;
+            while(zero>k){
+                if(arr[l]==0){
+                    zero--;
+                }
+                l++;
+            }
+            int len=r-l+1;
+            if(zero<=k){
+                maxLen=Math.max(maxLen,len);
+                r++;
+            }
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args) {
         int[] arr={1,1,1,0,0,0,1,1,1,1,0};
         MaxConsecutiveOnesIII sol=new MaxConsecutiveOnesIII();
-        System.out.println(sol.brute(arr,2));
+//        System.out.println(sol.brute(arr,2));
+        System.out.println(sol.better(arr,2));
     }
 }
