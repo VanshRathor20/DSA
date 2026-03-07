1376d035d8fcc75daff40ee758b984f4a384146e
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-07 22:52:46 +0530

    fruit into 2 basket (how many fruits is come in 2 basket ) better approach

--- src/SlidingWindow/FruitIntoBasket.java
+++ src/SlidingWindow/FruitIntoBasket.java
@@ -19,9 +19,29 @@ public class FruitIntoBasket {
         return max;
     }
 
+    // better
+    public static int better(int[] fruits){
+        int l=0,r=0,n=fruits.length,max=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(r<n){
+            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+            while(map.size()>2){
+                map.put(fruits[l],map.getOrDefault(fruits[l],0)-1);
+                if(map.get(fruits[l]) == 0){
+                    map.remove(fruits[l]);
+                }
+                l++;
+            }
+            max=Math.max(max,r-l+1);
+            r++;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         FruitIntoBasket sol=new FruitIntoBasket();
         int[] arr={3,3,3,1,2,1,1,2,3,3,4};
         System.out.println(sol.TotalFruitBrute(arr));
+        System.out.println(sol.better(arr));
     }
 }
