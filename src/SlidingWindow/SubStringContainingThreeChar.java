1773bbc780cbf45653387f1327c1035ee205abd8
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-13 22:56:33 +0530

    number of substring containing all three character optimal

--- src/SlidingWindow/SubStringContainingThreeChar.java
+++ src/SlidingWindow/SubStringContainingThreeChar.java
@@ -14,9 +14,26 @@ public class SubStringContainingThreeChar {
         }
         return cnt;
     }
+
+    //optimal
+    public static int optimal(String s){
+        int l=0,r=0,n=s.length(),cnt=0;
+        int[] hash=new int[3];
+        while(r<n){
+            hash[s.charAt(r)-'a']++;
+            while(hash[0]>0 && hash[1]>0 && hash[2]>0){
+                cnt=cnt+(n-r);
+                hash[s.charAt(l)-'a']--;
+                l++;
+            }
+            r++;
+        }
+        return cnt;
+    }
     public static void main(String[] args) {
         SubStringContainingThreeChar sol=new SubStringContainingThreeChar();
         String s="bbacba";
         System.out.println(sol.brute(s));
+        System.out.println(sol.optimal(s));
     }
 }
