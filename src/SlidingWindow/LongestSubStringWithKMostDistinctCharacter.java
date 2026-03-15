0bb0a2f0550763880beb71912d7cc787c3ea8782
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-15 14:58:52 +0530

    Longest SubString With K Most Distinct Character optimal

--- src/SlidingWindow/LongestSubStringWithKMostDistinctCharacter.java
+++ src/SlidingWindow/LongestSubStringWithKMostDistinctCharacter.java
@@ -18,9 +18,29 @@ public class LongestSubStringWithKMostDistinctCharacter {
         return maxLen;
     }
 
+    //optimal
+    public static int optimal(String s,int k){
+        int l=0,r=0,maxLen=0,n=s.length();
+        HashMap<Character,Integer> map=new HashMap<>();
+        if(s.length()==0 || k==0) return 0;
+        while(r<n){
+            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
+            while(map.size()>k){
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                if(map.get(s.charAt(l))==0){
+                    map.remove(s.charAt(l));
+                }
+                l++;
+            }
+            maxLen=Math.max(maxLen,(r-l+1));
+            r++;
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
         LongestSubStringWithKMostDistinctCharacter sol =new LongestSubStringWithKMostDistinctCharacter();
         String s="eceba";
         System.out.println(sol.brute(s,2));
+        System.out.println(sol.optimal(s,2));
     }
 }
