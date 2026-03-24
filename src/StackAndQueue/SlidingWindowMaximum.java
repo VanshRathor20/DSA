bdb776fd1e699a86ab0f96a43a842fae06aeaeab
VanshRathor20 <vanshrathor5887@gmail.com>
2026-03-24 23:10:47 +0530

    sliding window maximum optimal

--- src/StackAndQueue/SlidingWindowMaximum.java
+++ src/StackAndQueue/SlidingWindowMaximum.java
@@ -1,6 +1,8 @@
 package StackAndQueue;
 
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class SlidingWindowMaximum {
     public static int[] brute(int[] arr,int k){
@@ -16,10 +18,33 @@ public class SlidingWindowMaximum {
         return ans;
     }
 
+    //optimal
+    public static int[] optimal(int[] nums,int k){
+        Deque<Integer> dq = new LinkedList<>();
+        int n=nums.length;
+        // List to store result
+        int[] result = new int[n - k + 1];
+        int idx = 0;
+        for( int i=0;i<n;i++){
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() &&nums[dq.peekLast()] <= nums[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if(i>=k-1){
+                result[idx++] = nums[dq.peekFirst()];
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] arr={1,3,-1,-3,5,3,6,7};
         int k=3;
         System.out.println(Arrays.toString(brute(arr,k)));
+        System.out.println(Arrays.toString(optimal(arr,k)));
     }
 
 }
