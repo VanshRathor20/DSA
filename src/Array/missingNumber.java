18c35e9b5967a98d65d6ff370706539b6c2189ac
Vansh Rathor <vanshrathor5887@gmail.com>
2026-06-14 12:23:51 +0530

    update missig number

--- src/Array/missingNumber.java
+++ src/Array/missingNumber.java
@@ -51,10 +51,28 @@ public class missingNumber {
     }
 
 
+    //optimal using xor
+        public static int xor(int[] arr){
+            int l=arr.length+1;
+            int xor1=0;
+            for(int i=0;i<=l;i++){
+                xor1=xor1^i;
+            }
+            int xor2=0;
+            for(int i=0;i<arr.length;i++){
+                xor2=xor2^arr[i];
+            }
+            return xor1^xor2;
+
+        }
+
+
+
     public static void main(String[] args) {
         int[] arr={1,2,4,5};
-        System.out.println(findMissing(arr));
+//        System.out.println(findMissing(arr));
 //        System.out.println(missingNumber(arr));
 //        System.out.println(miss(arr));
+        System.out.println(xor(arr));
     }
 }
