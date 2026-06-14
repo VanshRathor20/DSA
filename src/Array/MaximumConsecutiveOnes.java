d34a8bb890ac24a3cb4e2b227c9c81d3d4b0d934
Vansh Rathor <vanshrathor5887@gmail.com>
2026-06-14 12:23:11 +0530

    update maxi consecutive ones

--- src/Array/MaximumConsecutiveOnes.java
+++ src/Array/MaximumConsecutiveOnes.java
@@ -8,11 +8,12 @@ public class MaximumConsecutiveOnes {
         for(int i=0;i<n;i++){
             if(arr[i]==1){
                 count++;
+                max=Math.max(max,count);
             }
             else{
                 count=0;
             }
-            max=Math.max(max,count);
+
         }
         return max;
     }
